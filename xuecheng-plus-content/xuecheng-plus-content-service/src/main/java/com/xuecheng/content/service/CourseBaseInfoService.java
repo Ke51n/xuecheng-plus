7b2b658a54866230c26dd6a6e65c00153fa92165
wangkai <739521138@qq.com>
2023-04-18 23:49:27 +0800

    day02 添加课程,编写接口并测试通过

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -2,6 +2,8 @@ package com.xuecheng.content.service;
 
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.AddCourseDto;
+import com.xuecheng.content.model.dto.CourseBaseInfoDto;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
 
@@ -14,7 +16,7 @@ import com.xuecheng.content.model.po.CourseBase;
  * @author wk
  * @version 1.0
  */
-public interface CourseBaseInfoService  {
+public interface CourseBaseInfoService {
 
     /*
      * @description 课程查询接口
@@ -23,5 +25,15 @@ public interface CourseBaseInfoService  {
      * @return com.xuecheng.base.model.PageResult<com.xuecheng.content.model.po.CourseBase>
      */
     PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
+
+    /**
+     * @param companyId    教学机构id
+     * @param addCourseDto 课程基本信息
+     * @return com.xuecheng.content.model.dto.CourseBaseInfoDto
+     * @description 添加课程基本信息
+     * @author Mr.M
+     * @date 2022/9/7 17:51
+     */
+    CourseBaseInfoDto createCourseBase(Long companyId, AddCourseDto addCourseDto);
 
 }
