56f90122f0a07e3707c6a07bd79e836b6a44c1d4
wangkai <739521138@qq.com>
2023-04-18 17:41:03 +0800

    day2 add CourseBaseInfoService test:mapper and service

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/XuechengPlusContentModelApplication.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/XuechengPlusContentModelApplication.java
@@ -1,13 +1,13 @@
-package com.xuecheng.content.model;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class XuechengPlusContentModelApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(XuechengPlusContentModelApplication.class, args);
-    }
-
-}
+//package com.xuecheng.content.service;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class XuechengPlusContentModelApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(XuechengPlusContentModelApplication.class, args);
+//    }
+//
+//}
---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/ContentApplication.java
@@ -0,0 +1,20 @@
+package com.xuecheng.content;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * package:  com.xuecheng.content
+ * project_name:  xuecheng-plus
+ * 2023/4/18  10:25
+ * description:
+ *
+ * @author wk
+ * @version 1.0
+ */
+@SpringBootApplication
+public class ContentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentApplication.class, args);
+    }
+}
