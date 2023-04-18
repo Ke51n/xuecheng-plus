bafaefc7e42638f1b4206768654939020b5392d8
wangkai <739521138@qq.com>
2023-04-18 19:16:58 +0800

    day02 后端接口、服务测试完成

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -4,16 +4,14 @@ import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * package:  com.xuecheng.content.api
@@ -27,13 +25,20 @@ import java.util.List;
 @Api(value = "课程信息管理接口", tags = "课程信息管理接口")
 @RestController
 public class CourseBaseInfoController {
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
+
     @ApiOperation("课程查询接口")
     @PostMapping("/course/list")
     public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto) {
         // RequestBody 前端请求的json字符串转为java对象
         // required = false,该json参数非必要，当用户不传参时，默认查询所有
 
-        CourseBase courseBase = new CourseBase();
+        //真实请求，测试接口实现类
+        PageResult<CourseBase> pageResult = courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParamsDto);
+
+        //模拟数据
+       /* CourseBase courseBase = new CourseBase();
         CourseBase courseBase1 = new CourseBase();
         courseBase.setName("测试名称11");
         courseBase1.setName("测试名称22");
@@ -43,6 +48,7 @@ public class CourseBaseInfoController {
         courseBases.add(courseBase);
         courseBases.add(courseBase1);
         PageResult pageResult = new PageResult<CourseBase>(courseBases, 10, 1, 10);
+        */
         return pageResult;
 //        return null;
     }
